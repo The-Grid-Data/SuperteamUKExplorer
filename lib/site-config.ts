8e24a39d10b22a327aebda6d9f0af1cc087520e9
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-10-08 12:36:54 +0000

    Update site config

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,7 +1,6 @@
 export const siteConfig = {
-  pageTitle: 'The Grid | Data Explorer',
-  pageDescription:
-    'Powering Discoverability of Assets, Products, and Services in Web3.',
+  pageTitle: 'SuperteamUK Directory | The Grid',
+  pageDescription: 'A list of SuperteamUK projects',
   filterByProductIds: [], // Used to filter which blockchains to show in the full list.
   blockchainIds: [], // Filter default options. Filters the options of (productSupports, productDeployedOnFilter and assetDeployedOnFilter)
   blockchainProductTypeIds: [15, 16, 17], // Filter default options. used by productDeployedOnFilter.
